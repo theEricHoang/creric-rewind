d611aee95408c52b4828562467eaacc065831364
theboysack <ehoang2@student.gsu.edu>
2025-02-09 21:30:45 -0500

    update better-auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
 
 const protectedRoutes = ['/notes'];
 
 export async function middleware(request: NextRequest) {
   if (protectedRoutes.includes(request.nextUrl.pathname)) {
-    if (!request.cookies.has("better-auth.session_token")) {
+    const sessionCookie = getSessionCookie(request);
+    if (!sessionCookie) {
       return NextResponse.redirect(new URL("/login", request.url));
     }
+    return NextResponse.next();
   }
 }
